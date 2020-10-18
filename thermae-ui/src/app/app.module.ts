d26760db4ddb9f8a4a897c6170196b83bd1369c7
fjavierm <fco.javier.marquez@gmail.com>
2020-10-18 10:23:57 +0100

    Add UI signup (Route not working)

--- thermae-ui/src/app/app.module.ts
+++ thermae-ui/src/app/app.module.ts
@@ -11,23 +11,28 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
